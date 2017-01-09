ed7998ed72899929c21648dbedc4382f56338328
Adam Orr <adamjorr@gmail.com>
2017-01-09 16:59:16 -0700

    work on getting bams openable and iterable

--- htsapi.cc
+++ htsapi.cc
@@ -1,5 +1,56 @@
+//TODO: be sure all pointers have the correct destructors from htslib
+//TODO: implement next
+
+HtsAPI(std::string filename){
+	open(filename);
+	this->region_exists = false;
+}
+
+HtsAPI(std::string filename, std::string region){
+	open(filename, region);
+	this->region_exists = true;
+}
 
 
+//set in and header
+static void HtsAPI::open(std::string filename_in){
+	if ((in = sam_open(filename_in.c_str(), "r")) == 0) {
+    	//error
+        throw std::runtime_error("error");
+    }
+    this->in = std::uniq_ptr(in);
+
+    if ((header = sam_hdr_read(in)) == 0){
+    	//error
+    	throw std::runtime_error("error");
+    }
+    this->header = std::uniq_ptr(header);
+}
+
+//set in and header and idx and iter
+static void HtsAPI::open(std::string filename_in, std::string region){
+	prep_open(filename_in);
+    std::uniq_ptr<hts_idx_t> idx(sam_index_load(in, filename_in.c_str()),&hts_idx_destroy)
+    if (idx != nullptr){
+    	//error
+    	throw std::runtime_error("error");
+    }
+    this->idx = idx;
+
+    std::uniq_ptr<hts_itr_t> iter(sam_itr_querys(idx.pointer, header, region.c_str()), &hts_itr_destroy);
+    if (iter != nullptr){
+    	throw std::runtime_error("error");
+    }
+    this->iter = iter;
+}
+
+int HtsAPI::next(bam1_t* b){
+	
+}
+
+bool HtsAPI::has_region(){
+	return this->region_exists;
+}
 
 //see https://github.com/samtools/samtools/blob/9df632/bam_md.c#L58-L90
 static bool HtsAPI::has_mismatch(bam1_t *b, char *ref, int ref_len){
@@ -15,38 +66,21 @@ static bool HtsAPI::has_mismatch(bam1_t *b, char *ref, int ref_len){
 				if (x + j >= ref_len || ref[x + j]] == '\0') break; //out of bounds
 				c1 = bam_seqi(seq, z);
 				c2 = seq_nt16_table[(int)ref[x+j]];
-				if ((c1 == c2 && c1 != 15 && c2 != 15) || c1 == 0){ //a match
+				if (!((c1 == c2 && c1 != 15 && c2 != 15) || c1 == 0)){ //doesn't match
 					return true
 				}
-				else{
-					return false
-				}
 			}
-			if (j < l) break
-			x += 1;
-			y += 1;
+			if (j < l) break //break again if we have to break because we're out of bounds
+			x += l;
+			y += l;
 		}
-		else if (op == BAM_CDEL){
-			return true
-		}
-		else if (op == BAM_CINS || op == BAM_CSOFT_CLIP){
-			return true
-		}
-		else if (op == BAM_CREFSKIP){
+		else{
 			return true
 		}
 	}
 }
 
-static bool HtsAPI::has_mismatch(bam1_t *b){
-	uint32_t *cigar = bam_get_cigar(b);
-	bam1_core_t *c = &b->core;
-	for (i = 0; i < c->n_cigar; ++i){
-		op = (cigar[i]>>4, cigar[i]&0xf);
-		if (op != BAM_CMATCH || op != BAM_CEQUAL){
-			return true
-		}
-	}
-	return false
-}
+
+
+
 
--- htsapi.h
+++ htsapi.h
@@ -1,10 +1,26 @@
 #ifndef __ERRLUMINA_HTS_API_INCLUDED__
 #define __ERRLUMINA_HTS_API_INCLUDED__
 
+#include <memory>
+#include <hts>
+
 class HtsAPI{
+protected:
+	std::uniq_ptr<sam_fh_t> in;
+	std::uniq_ptr<sam_hdr_t> header;
+	std::uniq_ptr<sam_idx_t> idx;
+	std::uniq_ptr<sam_itr_t> iter;
+	std::uniq_ptr<std::string> region;
+	bool region_exists;
+	void open(std::string);
+	void open(std::string, std::string);
 public:
-	static bool has_mismatch(bam1_t *b, char *ref, int ref_len)
-}
+	bool has_region();
+	int next(bam1_t *b);
+	HtsAPI(std::string filename);
+	HtsAPI(std::string filename, std::string region);
+	static bool has_mismatch(bam1_t *b, char *ref, int ref_len);
+};
 
 
 
